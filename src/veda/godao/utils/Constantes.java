c700c9a11fd74144846daa6e9ca44e633578ba2f
Eriq606 <erohweltall@gmail.com>
2023-12-11 08:18:30 +0100

    initial commit

---
+++ src/veda/godao/utils/Constantes.java
@@ -0,0 +1,10 @@
+package veda.godao.utils;
+public class Constantes{
+    public static final String TABLE_VALUE="value";
+    public static final String COLUMN_VALUE="value";
+
+    public static final int NOCONNECTION_CODE=1;
+
+    public static final int MYSQL_ID=1;
+    public static final int PSQL_ID=2;
+}
